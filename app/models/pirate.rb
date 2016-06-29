96e6a9c939893b002dbd6ed2bd6c5b0c80f35920
greatermeans <gakouri@gmail.com>
2016-06-29 10:52:59 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+	attr_accessor :name,:weight,:height
+
+	@@all = []
+
+	def initialize(opt)
+		@name = opt[:name]
+		@weight = opt[:weight]
+		@height = opt[:height]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.clear
+		@@all = []
+	end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+	attr_accessor :name, :type, :booty 
+
+	@@all = []
+
+	def initialize(opt)
+		@name = opt[:name]
+		@type = opt[:type]
+		@booty = opt[:booty]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.clear
+		@@all = []
+	end
 end
